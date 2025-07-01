e661e7c7890d7c8c0e920f522c0c465ae82a7102
gaurav20085 <gaurav20085@gmail.com>
2025-07-01 14:02:43 +0530

    feat:added user from module

---
+++ iam.tf
@@ -0,0 +1,10 @@
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  name          = "vasya.pupkin"
+  force_destroy = true
+
+  pgp_key = "keybase:test"
+
+  password_reset_required = false
+}
