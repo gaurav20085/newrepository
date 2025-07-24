fd20319a8c11494e65d71710d202d8ab5185af83
gaurav20085 <gaurav20085@gmail.com>
2025-07-24 16:44:03 +0530

    added node group

--- EKS/main.tf
+++ EKS/main.tf
@@ -10,21 +10,21 @@ module "eks" {
 
 }
 
-# module "eks_managed_node_group" {
-#   source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+module "eks_managed_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
 
-#    create = true
-#    name = "my-cluster"
-#    subnet_ids = var.subnet_ids
-#    cluster_name = module.eks.cluster_name
-#    #cluster_version = var.cluster_version
-#    #cluster_endpoint = module.eks
-#    min_size = var.min_size
-#    max_size = var.max_size
-#    disk_size = var.disk_size
-#    instance_types = var.instance_types
-#    capacity_type = var.capacity_type
-#    tags = var.tags
-#    cluster_service_cidr = var.cluster_service_cidr
-#    depends_on = [ module.eks ]
-# }
+   create = true
+   name = "my-cluster"
+   subnet_ids = var.subnet_ids
+   cluster_name = module.eks.cluster_name
+   #cluster_version = var.cluster_version
+   #cluster_endpoint = module.eks
+   min_size = var.min_size
+   max_size = var.max_size
+   disk_size = var.disk_size
+   instance_types = var.instance_types
+   capacity_type = var.capacity_type
+   tags = var.tags
+   cluster_service_cidr = var.cluster_service_cidr
+   depends_on = [ module.eks ]
+}
