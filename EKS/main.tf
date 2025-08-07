29d1c037e46c5514aee91cb881ffb0bb3b787451
gaurav20085 <gaurav20085@gmail.com>
2025-08-07 12:05:36 +0530

    cluster removed

--- EKS/main.tf
+++ EKS/main.tf
@@ -1,12 +1,12 @@
-module "eks" {
-    source = "terraform-aws-modules/eks/aws"
-    version = "~> 21.0"
+# module "eks" {
+#     source = "terraform-aws-modules/eks/aws"
+#     version = "~> 21.0"
 
-    name = var.cluster_name
-    kubernetes_version = var.cluster_version
-    vpc_id = var.vpc_id
-    subnet_ids = var.subnet_ids
+#     name = var.cluster_name
+#     kubernetes_version = var.cluster_version
+#     vpc_id = var.vpc_id
+#     subnet_ids = var.subnet_ids
 
-    create_cloudwatch_log_group = false
-    create_kms_key = true
-}
+#     create_cloudwatch_log_group = false
+#     create_kms_key = true
+# }
